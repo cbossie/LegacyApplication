570d47e70879453426fd238a59b19a7cf10b5243
cbossie <cbossie@gmail.com>
2021-11-15 18:03:12 -0500

    Transform

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+
+aws ecr get-login-password --region us-east-1 --profile aws-cbbossie | docker login --username AWS --password-stdin 020698190649.dkr.ecr.us-east-1.amazonaws.com
+
+
+docker build -t windows-fargate-legacy --build-arg source=. .\LegacyApplication
+docker tag windows-fargate-legacy:latest 020698190649.dkr.ecr.us-east-1.amazonaws.com/windows-fargate-legacy:latest
+docker push 020698190649.dkr.ecr.us-east-1.amazonaws.com/windows-fargate-legacy:latest
+
