7b2e29e92d78de36be43dca677136fb6e5fddf28
Craig Bossie <cbbossie@amazon.com>
2021-11-16 23:38:28 +0000

    Redis Session

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 020698190649.dkr.ecr.us-east-1.amazonaws.com
-docker build -t windows-fargate-legacy --build-arg source=. .\LegacyApplication
-docker tag windows-fargate-legacy:latest 020698190649.dkr.ecr.us-east-1.amazonaws.com/windows-fargate-legacy:latest
-docker push 020698190649.dkr.ecr.us-east-1.amazonaws.com/windows-fargate-legacy:latest
+docker build -t iis-dotnetframework --build-arg source=. .\LegacyApplication
+docker tag iis-dotnetframework:latest 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest
+docker push 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest
 
