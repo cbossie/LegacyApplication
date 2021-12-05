a5e71f347f6a04d5810ebc2ec3bb61b8923009c1
Craig Bossie <cbbossie@amazon.com>
2021-12-05 16:39:13 +0000

    Deploy Separate Base Image

--- deploy.bat
+++
@@ -1,6 +0,0 @@
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 020698190649.dkr.ecr.us-east-1.amazonaws.com
-docker build -t iis-dotnetframework --build-arg source=. .\LegacyApplication
-docker tag iis-dotnetframework:latest 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest
-docker push 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest
-
--- deploy_ec2.bat
+++
@@ -1,6 +0,0 @@
-
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 020698190649.dkr.ecr.us-east-1.amazonaws.com
-docker build -t iis-dotnetframework:latest-ec2 --build-arg source=. .\LegacyApplication
-docker tag iis-dotnetframework:latest-ec2 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest-ec2
-docker push 020698190649.dkr.ecr.us-east-1.amazonaws.com/iis-dotnetframework:latest-ec2
-
